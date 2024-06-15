0ca3d2ff70c7602249eacea3b8d00c73164b1b73
Shuyang Yao <qzyaoshuyang3@pku.edu.cn>
2024-06-15 22:36:35 +0800

    update. Add modifier which help denoise manually

---
+++ behav_concat_sessions.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import os
+import json
+from typing import Optional
+import matplotlib.pyplot as plt
+
--- local_path.py
+++ local_path.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 import os
 
-f1 = pd.read_excel(r"D:\YSY\data_list.xlsx")
-f1_behav = pd.read_excel(r"E:\behav\behavioral_paradigms.xlsx")
+f1 = pd.read_excel(r"E:\behav\behavioral_paradigms.xlsx", sheet_name='neuract')
+f1_behav = pd.read_excel(r"E:\behav\behavioral_paradigms.xlsx", sheet_name='behav')
